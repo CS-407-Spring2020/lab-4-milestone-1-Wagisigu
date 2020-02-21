88166ef0d0db0f2fe4ba9e82806c6505e8d1f895
Cameron Wagner <cjwagner4@wisc.edu>
2020-02-21 14:37:23 -0600

    milestone 1 fin

--- app/src/main/java/com/example/lab4milestone1/MainActivity.java
+++ app/src/main/java/com/example/lab4milestone1/MainActivity.java
@@ -3,12 +3,77 @@ package com.example.lab4milestone1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    class ExampleRunnable implements Runnable {
+
+        int seconds;
+
+        ExampleRunnable(int s) {
+            seconds = s;
+        }
+
+        @Override
+        public void run() {
+            for (int i = 0; i < seconds; i++) {
+                if (stopThread) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            startButton.setText(getString(R.string.startButton_text));
+                        }
+                    });
+                    return;
+                }
+                if (i == 5) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            startButton.setText(getString(R.string.startButton_text50));
+                        }
+                    });
+                }
+                Log.d(TAG,"startThread: "+i);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    startButton.setText(getString(R.string.startButton_text));
+                }
+            });
+        }
+    }
+
+    private static final String TAG = "MainActivity";
+
+    private volatile boolean stopThread = false;
+
+    private Button startButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startButton = findViewById(R.id.startButton);
+    }
+
+    public void startThread(View view) {
+        stopThread = false;
+        ExampleRunnable runnable = new ExampleRunnable(10);
+        new Thread(runnable).start();
+    }
+
+    public void stopThread(View view) {
+        stopThread = true;
     }
 }
